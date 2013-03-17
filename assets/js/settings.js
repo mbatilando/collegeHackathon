fb56e5d14405e8888368e9b3ca1623a7546e64aa
kevtchan <kevtchan@gmail.com>
2013-03-17 15:52:27 -0700

    settings page work

--- assets/js/settings.js
+++ assets/js/settings.js
@@ -7,4 +7,18 @@ $(document).ready(function() {
 			$(this).find('span').html('&#x25BC;');
 		}
 	});
+	var toggleFunction = function(item) {
+		var parent = item.parents('li').first();
+		if(parent.hasClass('header') == false) {
+			parent.toggleClass('selected');
+		}
+	};
+	$('label').click(function(e) {
+		toggleFunction($(this));
+		
+	});
+	$('a').click(function(e) {
+		toggleFunction($(this));
+		
+	});
 });
